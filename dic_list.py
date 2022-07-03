d7092d68fd5800ebf09b81eb8c96ce5663c86bdb
dhyuk5 <dhyuk23@gmail.com>
2022-07-03 20:52:56 +0900

    list sum practise

--- dic_list.py
+++ dic_list.py
@@ -2,6 +2,8 @@ numbers = {'dozens':[10,20,40],'hundred]':[100,101,120,140]}
 #print(numbers['dozens'])
 #print(numbers['dozens'][1])
 
+# 70
+print(sum(numbers['dozens']))
 """
 [10, 20, 40]
 20
@@ -32,7 +34,7 @@ pen
 orange
 {'apple': 0, 'pen': 1, 'orange': 2}
 """
-
+##
 
 def handle_collision(dic1, str1):
     if dic1.get(len(str1)) is None:
@@ -46,4 +48,22 @@ handle_collision({3: ['ham', 'egg'], 6: ['coffee', 'brandy'], 9: ['port wine'], 
 dic2 = {3: ['ham', 'egg'], 6: ['coffee', 'brandy'], 9: ['port wine'], 15: ['curried chicken']}
 
 str = 'tea'
-print([str]) # ['tea']
+#print([str]) # ['tea']
+
+a = [[3,5,6],2,3,5]
+#print(type(a))
+s = 0
+for i in a:
+    if type(i) is list:
+        for j in i:
+            if type(j) is int:
+                s += j
+        print("list_sum",s)
+    elif type(i) is int:
+        s += i
+print("total_sum",s)
+
+"""
+list_sum 14
+total_sum 24
+"""
