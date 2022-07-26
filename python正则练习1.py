e246d11271029be5b7345bf81f841692576f2980
dhyuk5 <dhyuk23@gmail.com>
2022-07-26 23:37:22 +0900

    python正则练习 前后有数字时取字符,前后有正负号时都取

--- python正则练习1.py
+++ python正则练习1.py
@@ -1,10 +1,11 @@
 
 import pandas as pd
 import re
+import numpy as np
 
 
-df = pd.DataFrame({'mileage': ['1無名1', '1無名', '無名1'],
-                   'engine': ['1.4liter', '1.2liter', '1.5liter'],
+df = pd.DataFrame({'mileage': ['1無名1', '1無名', '無名1', '無名1', '無名1', '無名1'],
+                   'engine': ['-1000-', '1000-', '1000+', 'aaaa', 'bbbb', 'ccccc'],
                    })
 # print(df)
 """
@@ -14,22 +15,43 @@ df = pd.DataFrame({'mileage': ['1無名1', '1無名', '無名1'],
 2    k無名1  1.5 liter
 """
 
-
 def keep_behind_digital(x):
     try:
-        # result = re.findall(r'[\D]+', x)
+        #result = re.findall(r'[\D]+', x)
         result = re.findall(r'[^0-9]+', x)
+
         return result
     except Exception :
         print("三种都没匹配")
 
 
+def keep_behind_digital_1(x):
+        result = re.findall(r'[-+0-9]+', x)
+        if result == []:
+            return np.nan
+        return result
+
+
 df['mileage'] = df['mileage'].apply(keep_behind_digital)
+df['engine'] = df['engine'].apply(keep_behind_digital_1)
 print(df['mileage'])
 """
 0    [無名]
 1    [無名]
 2    [無名]
+3    [無名]
+4    [無名]
+5    [無名]
 Name: mileage, dtype: object
+"""
+print(df['engine'])
+"""
+0    [-1000-]
+1     [1000-]
+2     [1000+]
+3         NaN
+4         NaN
+5         NaN
+Name: engine, dtype: object
 """
 
